95102e55f2e3c96ab7540d9cfcd026db805d1baf
Mohamed Shihaab <mohameedshihaab@gmail.com>
2026-03-30 16:57:40 +0530

    Fix formatting and improve ASCII value program

--- ASCII value of a given character..py
+++ ASCII value of a given character..py
@@ -1,4 +1,5 @@
 # Write a python program to display the ASCII value of a given character.
+
 char=input("Enter a Character ")
 ascii_value = ord(char)
 print("The ASCII value of ",char,"is",ascii_value)
