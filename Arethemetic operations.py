e0219e7aa81aad3b61769d449ba8ff593285b371
Mohamed Shihaab <mohameedshihaab@gmail.com>
2026-04-09 21:45:52 +0530

    Update and rename addition 3.py to Arethemetic operations.py

---
+++ Arethemetic operations.py
@@ -0,0 +1,11 @@
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+sum = 'a+b'
+sub = 'a-b'
+product ='a*b'
+div = 'a/b''
+
+print("sum of a and b is : ",a+b)
+print("subtraction of a and b is : ")
+print("product of a and b is : ",a*b)
+print("divition of a and b is ; ",a/b)
